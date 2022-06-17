00dd263bd1b4ba2e119722e9bcdbcda01c636ed3
toda <ytoda@d-forest-corp.com>
2022-06-17 17:45:52 +0900

    Setting Next JS

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className='p-4'>
+      <Head>
+        <title>Hello! Next Tailwind!</title>
+      </Head>
+      <h1 className='text-3xl font-bold underline'>
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default Home
