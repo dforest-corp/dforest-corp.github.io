72767e225614c933ee033a41047e2bdf82df2aac
toda <ytoda@d-forest-corp.com>
2022-06-18 10:46:01 +0900

    ドロップダウンメニューの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,30 @@
 import Link from 'next/link'
+import DropDownMenu from './Home/DropDownMenu'
 
 const Header = () => {
   return (
-    <header className='p-6'>
+    <header className='p-6 flex flex-row'>
       <h1 className='text-3xl font-bold text-green-800 italic'>
         <Link href='/'>D-FOREST</Link>
       </h1>
+      <ul className='hidden md:flex flex-row ml-auto space-x-6 text-lg text-gray-800'>
+        <li>
+          <Link href=''>
+            会社案内
+          </Link>
+        </li>
+        <li>
+          <Link href=''>
+            事業内容
+          </Link>
+        </li>
+        <li>
+          <Link href=''>
+            お問い合わせ
+          </Link>
+        </li>
+      </ul>
+      <DropDownMenu />
     </header>
   )
 }
---
+++ src/components/Home/DropDownMenu.tsx
@@ -0,0 +1,66 @@
+import {Menu, Transition} from '@headlessui/react'
+import {MdMenu} from 'react-icons/md'
+import Link, {LinkProps} from 'next/link'
+import {ComponentPropsWithoutRef, ReactNode} from 'react'
+
+type NextLinkProps = LinkProps & Omit<ComponentPropsWithoutRef<'a'>, 'href'>
+
+const NextLink = ({href, children, ...rest}: NextLinkProps) => (
+  <Link href={href}>
+    <a {...rest}>{children}</a>
+  </Link>
+)
+
+type MenuItem = {
+  href: LinkProps['href']
+  children: ReactNode
+}
+
+const DropDownMenuItem = ({href, children}: MenuItem) => (
+  <Menu.Item>
+    {({active}) => (
+      <NextLink href={href} className={`${
+        active ? 'bg-green-700 text-white' : 'text-gray-900'
+      } group flex w-full items-center rounded-md px-2 py-2 text-sm transition`}>
+        {children}
+      </NextLink>
+    )}
+  </Menu.Item>
+)
+
+const DropDownMenu = () => {
+  return (
+    <div className='block md:hidden ml-auto flex items-center'>
+      <Menu>
+        <Menu.Button className='focus:outline-none focus-visible:ring-0'>
+          <MdMenu size={30} />
+        </Menu.Button>
+        <Transition
+          enter='transition duration-100 ease-out'
+          enterFrom='transform scale-95 opacity-0'
+          enterTo='transform scale-100 opacity-100'
+          leave='transition duration-75 ease-out'
+          leaveFrom='transform scale-100 opacity-100'
+          leaveTo='transform scale-95 opacity-0'
+        >
+          <Menu.Items
+            className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+            <div className='px-1 py-1 '>
+              <DropDownMenuItem href='/'>
+                会社案内
+              </DropDownMenuItem>
+              <DropDownMenuItem href='/'>
+                事業内容
+              </DropDownMenuItem>
+              <DropDownMenuItem href='/'>
+                お問い合せ
+              </DropDownMenuItem>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  )
+}
+
+export default DropDownMenu
