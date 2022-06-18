704007f84ba9eeaf8413ef611f5e5430f5f5bcfc
toda <ytoda@d-forest-corp.com>
2022-06-18 11:17:21 +0900

    マージン設定

--- src/components/Home/ShopAppInformation.tsx
+++ src/components/Home/ShopAppInformation.tsx
@@ -4,12 +4,12 @@ const ShopAppInformation = () => {
   return (
     <div className='text-center px-2 py-20 bg-green-50'>
       <h3 className='text-2xl lg:text-4xl font-bold xl:font-black tracking-wider'>ショップdeアプリ</h3>
-      <p className='mt-1 text-lg tracking-wider'>簡単操作で集客＆リピーターup!</p>
-      <p className='mt-4 tracking-wider leading-relaxed'>
+      <p className='mt-2 text-lg tracking-wider'>簡単操作で集客＆リピーターup!</p>
+      <p className='mt-6 tracking-wider leading-relaxed'>
         恋する集客・販促アプリ「ショップdeアプリ」でどんどん”ラブレター”を送り、<br />
         お店に心と足を向けてもらいましょう。
       </p>
-      <div className='mt-4'>
+      <div className='mt-8'>
         <Image src='/images/shop-app.webp'
                alt='ショップdeアプリ'
                className='rounded-lg shadow-lg'
