6ee6af9264da7517c28307438054bb47fc6a9321
toda <ytoda@d-forest-corp.com>
2022-06-22 15:08:15 +0900

    トップ画像の変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='mt-20 py-20 bg-gray-800 text-white'>
+    <footer className='mt-20 py-20 bg-emerald-900 text-white'>
       <p className='text-center px-2'>
         Copyright &copy; 2022 D-FOREST CORP. All rights reserved.
       </p>
