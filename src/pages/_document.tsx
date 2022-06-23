e4aeac974a349161fa03e312a2d4ff989f1f18a7
toda <ytoda@d-forest-corp.com>
2022-06-23 10:29:51 +0900

    ファビコン設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
   return (
     <Html lang='ja'>
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#084E3B"/>
+        <link rel="shortcut icon" href="/favicon.svg" sizes="any" type="image/svg+xml"/>
+        <link rel="mask-icon" href="/favicon.svg" color="#084E3B"/>
+      </Head>
       <body>
       <Main />
       <NextScript />
