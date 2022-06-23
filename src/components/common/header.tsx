98167263ad4f9a72ab3a867843388b02262f6ee9
toda <ytoda@d-forest-corp.com>
2022-06-23 15:01:53 +0900

    リンク修正

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -6,21 +6,21 @@ const Header = () => {
     <header>
       <div className='py-6 px-4 xl:px-0 flex flex-row max-w-screen-xl mx-auto'>
         <h1 className='text-3xl font-bold text-green-800 italic'>
-          <Link href='/src/pages'>D-FOREST</Link>
+          <Link href='/'>D-FOREST</Link>
         </h1>
         <ul className='hidden md:flex flex-row ml-auto space-x-6 text-lg text-gray-800'>
           <li>
-            <Link href='/src/pages/company'>
+            <Link href='/company'>
               会社案内
             </Link>
           </li>
           <li>
-            <Link href='/src/pages/works'>
+            <Link href='/works'>
               事業内容
             </Link>
           </li>
           <li>
-            <Link href='/src/pages/contacts'>
+            <Link href='/contacts'>
               お問い合わせ
             </Link>
           </li>
