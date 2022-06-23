645c39848eb89b6c567c39f7b7dfc424940a1b67
toda <ytoda@d-forest-corp.com>
2022-06-23 14:48:32 +0900

    google fonts記述をdocumentsに移動

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html lang='ja'>
       <Head>
-        <meta name="theme-color" content="#084E3B"/>
-        <link rel="shortcut icon" href="/favicon.svg" sizes="any" type="image/svg+xml"/>
-        <link rel="mask-icon" href="/favicon.svg" color="#084E3B"/>
+        <meta name='theme-color' content='#084E3B' />
+        <link rel='shortcut icon' href='/favicon.svg' sizes='any' type='image/svg+xml' />
+        <link rel='mask-icon' href='/favicon.svg' color='#084E3B' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+        <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;900&display=swap'
+              rel='stylesheet' />
       </Head>
       <body>
       <Main />
