114617eb689fa62c5582f9d50c600d90032f682c
toda <ytoda@d-forest-corp.com>
2024-02-08 14:19:24 +0900

    set refresh

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700;900&display=swap'
               rel='stylesheet' />
+        <meta httpEquiv='refresh' content='0;URL=https://dforest-corp.vercel.app'/>
       </Head>
       <body>
       <Main />
